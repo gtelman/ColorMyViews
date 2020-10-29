cf135fc30374dd029ce642edc5b987c490a0072a
Gabrielle Telman <gabrielletelman@gmail.com>
2020-10-29 20:55:42 -0300

    adding background color functions

--- app/src/main/java/com/wcc/colormyviews/MainActivity.kt
+++ app/src/main/java/com/wcc/colormyviews/MainActivity.kt
@@ -3,14 +3,24 @@ package com.wcc.colormyviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var currentColor = R.color.grey
-
+    //var boxOneColor =
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setColorButtonsAction()
+        setBoxColor()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        //salvar na sharedPreferences cor da caixa um
     }
 
     private fun setColorButtonsAction(){
@@ -24,6 +34,24 @@ class MainActivity : AppCompatActivity() {
             currentColor = R.color.green
         }
 
+    }
+
+    private fun setBoxColor(){
+        box_one_text.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+        }
+        box_two_text.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+        }
+        box_three_text.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+        }
+        box_four_text.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+        }
+        box_five_text.setOnClickListener{
+            it.setBackgroundResource(currentColor)
+        }
     }
 
 

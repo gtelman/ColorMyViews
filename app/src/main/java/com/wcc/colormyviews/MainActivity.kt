94221f4846d9c551bbaa8db687f7ec0aeadcacdf
Gabrielle Telman <gabrielletelman@gmail.com>
2020-10-31 16:47:54 -0300

    using shared preferences

--- app/src/main/java/com/wcc/colormyviews/MainActivity.kt
+++ app/src/main/java/com/wcc/colormyviews/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wcc.colormyviews
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,24 +9,31 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var currentColor = R.color.grey
+    var boxOneColor = R.color.grey
     //var boxOneColor =
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sharedPreferences = this.getSharedPreferences("colors", Context.MODE_PRIVATE)
+        val color = sharedPreferences.getInt("color_box_one", R.color.grey)
+        box_one_text.setBackgroundColor(color)
         setColorButtonsAction()
         setBoxColor()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        //salvar na sharedPreferences cor da caixa um
+    override fun onStop() {
+        super.onStop()
+        val sharedPreferences = this.getSharedPreferences("colors", Context.MODE_PRIVATE)
+        val sharedPreferencesEditor = sharedPreferences.edit()
+        sharedPreferencesEditor.putInt("color_box_one", boxOneColor)
+        sharedPreferencesEditor.apply()
     }
 
     private fun setColorButtonsAction(){
         redButton.setOnClickListener {
             currentColor = R.color.red
+
         }
         yellowButton.setOnClickListener{
             currentColor = R.color.yellow
@@ -36,9 +44,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun saveBoxOneBackground(){
+
+        val background = box_one_text.background
+        /*
+        * acessar a cor de fundo definida em uma caixa quando o app for finalixado
+        * -sbrir o share preferences
+        * salvar cor da caixa um
+        * */
+    }
+
     private fun setBoxColor(){
         box_one_text.setOnClickListener{
             it.setBackgroundResource(currentColor)
+            boxOneColor = currentColor
         }
         box_two_text.setOnClickListener{
             it.setBackgroundResource(currentColor)
